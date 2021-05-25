7b0175e566e40d327e557a9c1dbe8704415e417d
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-05-24 21:17:38 -0600

    added pagination to merchant endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::MerchantsController < ApplicationController
+
   def index
-    render json: Merchant.all.limit(20)
+    
+    merchants_per_page = 20
+    page = params.fetch(:page, 0).to_i
+    render json: Merchant.limit(merchants_per_page).offset(page * merchants_per_page)
+
   end
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,19 +1,31 @@
 require 'rails_helper'
 
 describe 'Merchants' do
-  it 'sends a list of Merchants' do
-    create_list(:merchant, 25)
+  describe 'Happy Path' do
+    it 'sends a list of Merchants without a page request' do
+      create_list(:merchant, 25)
 
-    get '/api/v1/merchants'
+      get '/api/v1/merchants'
 
-    expect(response).to be_successful
-    merchants = JSON.parse(response.body)
-    expect(merchants.count).to eq(20)
-    merchants.each do |merchant|
-      expect(merchant).to have_key('name')
-      expect(merchant['name']).to be_an(String)
-      expect(merchant).to have_key('id')
-      expect(merchant['id']).to be_an(Integer)
+      expect(response).to be_successful
+      merchants = JSON.parse(response.body)
+      expect(merchants.count).to eq(20)
+      merchants.each do |merchant|
+        expect(merchant).to have_key('name')
+        expect(merchant['name']).to be_an(String)
+        expect(merchant).to have_key('id')
+        expect(merchant['id']).to be_an(Integer)
+      end
+    end
+
+    it 'sends a list of Merchants with a page request, and returns expected volume' do
+      create_list(:merchant, 50)
+
+      get '/api/v1/merchants', params:{page: 2}
+
+      expect(response).to be_successful
+      merchants = JSON.parse(response.body)
+      expect(merchants.count).to eq(10)
     end
   end
 end
