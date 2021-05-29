0295ad38c09d0c08f9b7961b101fea53b4687045
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-05-29 11:52:36 -0600

    refactored item search to be more readable

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,44 +1,53 @@
 class Api::V1::SearchController < ApplicationController
   def item_search
-    if params.key?(:name)
-      item = Item.find_by_name(params[:name])
-
-      if item.nil?
-
-        render json: ItemSerializer.new(Item.new), status: :not_found
-      else
-        render json: ItemSerializer.new(item)
-      end
+    if params.key?(:name) && !params.key?(:min_price) && !params.key?(:max_price)
+      name_search
     elsif params.key?(:max_price) && params.key?(:min_price)
-
-      item = Item.find_by_price(params[:min_price], params[:max_price])
-      if item.nil?
-        render json: ItemSerializer.new(Item.new), status: :not_found
-      else
-        render json: ItemSerializer.new(item)
-      end
+      max_min_search
     elsif params.key?(:max_price) && params[:max_price].to_f.positive?
-
-      item = Item.find_by_price_max(params[:max_price])
-      if item.nil?
-        render json: ItemSerializer.new(Item.new), status: :not_found
-      else
-
-        render json: ItemSerializer.new(item)
-      end
+      max_search
     elsif params.key?(:min_price) && params[:min_price].to_f.positive?
-      item = Item.find_by_price_min(params[:min_price])
-
-      if item.nil?
-        render json: ItemSerializer.new(Item.new), status: :not_found
-
-      else
-        render json: ItemSerializer.new(item)
-      end
+      min_seach
     else
-
       render json: { error: 'search params incorrect' }, status: :bad_request
+    end
+  end
 
+  def name_search
+    item = Item.find_by_name(params[:name])
+    if item.nil?
+      render json: ItemSerializer.new(Item.new), status: :not_found
+    else
+      render json: ItemSerializer.new(item)
+    end
+  end
+
+  def max_min_search
+    item = Item.find_by_price(params[:min_price], params[:max_price])
+    if item.nil?
+      render json: ItemSerializer.new(Item.new), status: :not_found
+    else
+      render json: ItemSerializer.new(item)
+    end
+  end
+
+  def max_search
+    item = Item.find_by_price_max(params[:max_price])
+    if item.nil?
+      render json: ItemSerializer.new(Item.new), status: :not_found
+    else
+      render json: ItemSerializer.new(item)
+    end
+  end
+
+  def min_seach
+    item = Item.find_by_price_min(params[:min_price])
+
+    if item.nil?
+      render json: ItemSerializer.new(Item.new), status: :not_found
+
+    else
+      render json: ItemSerializer.new(item)
     end
   end
 
